0af40d69d0df4382c3a44ac08282b24083b37332
shant0786 <rm.shanto786@gmail.com>
2026-01-20 09:02:49 +0600

    intial-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ShipmentsEntry from "./components/shipments-entry/ShipmentsEntry";
+
+function App() {
+  return (
+    <>
+      <ShipmentsEntry />
+    </>
+  );
+}
+
+export default App;
